7f362cea13d087ac3131cb5b0a804ed3705897d7
Ian Field <ian.field90@gmail.com>
2014-02-14 12:06:53 +0000

    Add fresh rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeachReach::Application.config.secret_key_base = 'cb086ffe9d5fc09bc7fcb7c1df0398f176e29003cae6c296655d0fe1c6777475060a9e5a19afaf12503a1e3e85beb2da3e5342a859f706e19f275a1a016a3e9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeachReach::Application.config.session_store :cookie_store, key: '_TeachReach_session'
