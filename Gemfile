83ede42de88a893d565ff45a137e51ef2188ec45
Ian Field <ian.field90@gmail.com>
2016-11-13 09:46:31 +0000

    Update to Rails 5

--- Gemfile
+++ Gemfile
@@ -1,18 +1,18 @@
 source 'https://rubygems.org'
 
-ruby "2.1.5"
+ruby "2.3.1"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2'
+gem 'rails', '5.0'
 
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails', '~> 5.0.0'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.2.1'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
@@ -47,7 +47,7 @@ group :development, :test do
   gem "factory_girl_rails"
 
   # Testing with rspec
-  gem 'rspec-rails', '~> 2.0'
+  gem 'rspec-rails', '~> 3.5.2'
   gem 'minitest'
 
   # Test coverage - see https://github.com/colszowka/simplecov/issues/281 for
@@ -71,5 +71,4 @@ gem 'cancancan', '~> 1.10'
 
 gem 'simple_form'
 
-gem 'bootstrap-sass', '~> 3.1.1'
-
+gem 'bootstrap-sass', '~> 3.3.7'
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -27,9 +26,8 @@ ActiveRecord::Schema.define(version: 20151201101143) do
     t.integer  "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_topics_on_subject_id", using: :btree
   end
-
-  add_index "topics", ["subject_id"], name: "index_topics_on_subject_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -44,10 +42,9 @@ ActiveRecord::Schema.define(version: 20151201101143) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "topics", "subjects"
 end
